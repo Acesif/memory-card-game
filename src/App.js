d83584f71800cd56cc79014f12e298d6e30545d4
Acesif <asifpalak@gmail.com>
2023-04-12 23:36:40 +0600

    added card images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import './App.css';
 
+const cardImages = [
+  {"src":"/images/F-1.png"},
+  {"src":"/images/F-2.png"},
+  {"src":"/images/W-1.png"},
+  {"src":"/images/W-2.png"},
+  {"src":"/images/G-1.png"},
+  {"src":"/images/G-2.png"},
+]
+
 function App() {
   return (
     <div className="App">
