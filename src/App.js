0bdf8b8d4c17b6c1ef2d9c913f270cac87496c44
Acesif <asifpalak@gmail.com>
2023-04-18 01:09:31 +0600

    modified img src

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Card from './components/Card';
 
 const cardImages = [
-  {"src":"./memory-card-game/images/F-1.png", match: false},
-  {"src":"./memory-card-game/images/F-2.png", match: false},
-  {"src":"./memory-card-game/images/W-1.p ng", match: false},
-  {"src":"./memory-card-game/images/W-2.png", match: false},
-  {"src":"./memory-card-game/images/G-1.png", match: false},
-  {"src":"./memory-card-game/images/G-2.png", match: false},
+  {"src":".public/images/F-1.png", match: false},
+  {"src":".public/images/F-2.png", match: false},
+  {"src":".public/images/W-1.p ng", match: false},
+  {"src":".public/images/W-2.png", match: false},
+  {"src":".public/images/G-1.png", match: false},
+  {"src":".public/images/G-2.png", match: false},
 ]
 
 function App() {
