b3b438244cd14a94c519bd5db2318cb25f17b365
Acesif <asifpalak@gmail.com>
2023-04-18 17:44:04 +0600

    updated readme

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Card from './components/Card';
 const cardImages = [
   {"src":"./images/F-1.png", match: false},
   {"src":"./images/F-2.png", match: false},
-  {"src":"./images/W-1.p ng", match: false},
+  {"src":"./images/W-1.png", match: false},
   {"src":"./images/W-2.png", match: false},
   {"src":"./images/G-1.png", match: false},
   {"src":"./images/G-2.png", match: false},
